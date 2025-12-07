4e07bc2e03ce9fee6d5f9589989361a69ce72ee0
Frank Flitton <fflitton@gmail.com>
2025-12-06 16:28:47 -0800

    Day 2

--- days/day02/a.ts
+++ days/day02/a.ts
@@ -3,12 +3,36 @@ import { readInput } from "../../utils";
 const input = await readInput(import.meta.dir);
 
 // Part A solution
-function solvePartA(data: string): number {
-  const lines = data.trim().split("\n");
+function solvePartA(rawData: string): number {
+  const cleanLines = rawData.replace(/\r/g, "") || "";
 
-  // Your solution here
+  const lines = cleanLines.split("\n").at(0) || "";
 
-  return 0;
+  const data = lines
+    .split(",")
+    .map((x) => {
+      const [minRaw, maxRaw] = x.split("-");
+      const minimum = parseInt(minRaw || "");
+      const maximum = parseInt(maxRaw || "");
+      return Array(maximum - minimum + 1)
+        .fill(0)
+        .map((_, i) => i + minimum);
+    })
+    .flat();
+
+  let count = 0;
+  for (const num of data) {
+    const str = num.toString();
+
+    if (
+      str.length % 2 == 0 &&
+      str.slice(0, str.length / 2) == str.slice(str.length / 2)
+    ) {
+      count += num;
+    }
+  }
+
+  return count;
 }
 
 const answer = solvePartA(input);
--- days/day02/b.ts
+++ days/day02/b.ts
@@ -2,16 +2,48 @@ import { readInput } from "../../utils";
 
 const input = await readInput(import.meta.dir);
 
-// Part B solution
-function solvePartB(data: string): number {
-  const lines = data.trim().split("\n");
+// Part A solution
+function solvePartA(rawData: string): number {
+  const cleanLines = rawData.replace(/\r/g, "") || "";
 
-  // Your solution here
+  const lines = cleanLines.split("\n").at(0) || "";
 
-  return 0;
+  const data = lines
+    .split(",")
+    .map((x) => {
+      const [minRaw, maxRaw] = x.split("-");
+      const minimum = parseInt(minRaw || "");
+      const maximum = parseInt(maxRaw || "");
+      const arr = [];
+      for (let index = 0; index < maximum - minimum + 1; index++) {
+        arr.push(index + minimum);
+      }
+      return arr;
+    })
+    .flat();
+
+  let count = 0;
+
+  for (const num of data) {
+    const str = num.toString();
+    for (let i = 1; i <= str.length / 2; i++) {
+      if (str.length % i == 0) {
+        let j = i;
+        for (; j < str.length; j += i) {
+          if (str.slice(j - i, j) != str.slice(j, j + i)) break;
+        }
+        if (j >= str.length) {
+          count += num;
+          break;
+        }
+      }
+    }
+  }
+
+  return count;
 }
 
-const answer = solvePartB(input);
-console.log("Part B:", answer);
+const answer = solvePartA(input);
+console.log("Part A:", answer);
 
-export { solvePartB };
+export { solvePartA };
