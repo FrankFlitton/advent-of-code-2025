6a53e6cc8d4fef438a9013a76cf757e0bac1e493
Frank Flitton <fflitton@gmail.com>
2025-12-06 23:01:09 -0800

    day 3

--- days/day03/a.ts
+++ days/day03/a.ts
@@ -4,11 +4,38 @@ const input = await readInput(import.meta.dir);
 
 // Part A solution
 function solvePartA(data: string): number {
-  const lines = data.trim().split("\n");
+  const batteryBanks = data
+    .trim()
+    .split("\n")
+    .map((l) => {
+      return l.split("").map((n) => parseInt(n));
+    });
 
   // Your solution here
 
-  return 0;
+  let curr = 0;
+  for (let index = 0; index < batteryBanks.length; index++) {
+    let largest = 0;
+    const bank = batteryBanks.at(index) || [];
+
+    // find first
+    for (let b1 = 0; b1 < bank.length - 1; b1++) {
+      const battery1 = bank?.at(b1) || 0;
+      // find second
+      for (let b2 = b1 + 1; b2 < bank.length; b2++) {
+        const battery2 = bank?.at(b2) || 0;
+        const curr = parseInt(`${battery1}${battery2}`);
+
+        if (curr > largest) {
+          largest = curr;
+        }
+      }
+    }
+
+    curr += largest;
+  }
+
+  return curr;
 }
 
 const answer = solvePartA(input);
--- days/day03/b.ts
+++ days/day03/b.ts
@@ -4,11 +4,34 @@ const input = await readInput(import.meta.dir);
 
 // Part B solution
 function solvePartB(data: string): number {
-  const lines = data.trim().split("\n");
+  const batteryBanks = data
+    .trim()
+    .split("\n")
+    .map((l) => {
+      return l.split("").map((n) => parseInt(n));
+    });
 
-  // Your solution here
+  let curr = 0;
+  for (const line of batteryBanks) {
+    let maxes = Array(12).fill(0);
+    for (let i = 0; i < line.length; i++) {
+      const joltage = line.at(i) || 0;
 
-  return 0;
+      for (let j = 0; j < maxes.length; j++) {
+        if (joltage > maxes[j] && i < line.length - maxes.length + j + 1) {
+          maxes[j] = joltage;
+
+          for (let k = j + 1; k < maxes.length; k++) {
+            maxes[k] = 0;
+          }
+          break;
+        }
+      }
+    }
+    curr += maxes.reduce((a, x, i, r) => a + x * 10 ** (r.length - i - 1), 0);
+  }
+
+  return curr;
 }
 
 const answer = solvePartB(input);
