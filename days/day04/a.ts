1960aed1ac785a4a89e4f2bf6a8807b5c803d9d4
Frank Flitton <fflitton@gmail.com>
2025-12-11 17:51:13 -0800

    Day 4

--- days/day04/a.ts
+++ days/day04/a.ts
@@ -4,11 +4,32 @@ const input = await readInput(import.meta.dir);
 
 // Part A solution
 function solvePartA(data: string): number {
-  const lines = data.trim().split("\n");
+  const input = data.trim().split("\n");
 
-  // Your solution here
+  let count = 0;
+  for (let y = 0; y < input.length; y++) {
+    const row = input[y] || "";
+    const top: string = y > 0 ? input[y - 1] || "" : "";
+    const bottom: string = y < input.length - 1 ? input[y + 1] || "" : "";
+    for (let x = 0; x < row.length; x++) {
+      if (row[x] == "@") {
+        let rolls = 0;
+        const left = x - 1;
+        const right = x + 1;
+        if (top[left] == "@") rolls++;
+        if (top[x] == "@") rolls++;
+        if (top[right] == "@") rolls++;
+        if (row[left] == "@") rolls++;
+        if (row[right] == "@") rolls++;
+        if (bottom[left] == "@") rolls++;
+        if (bottom[x] == "@") rolls++;
+        if (bottom[right] == "@") rolls++;
+        if (rolls < 4) count++;
+      }
+    }
+  }
 
-  return 0;
+  return count;
 }
 
 const answer = solvePartA(input);
--- days/day04/b.ts
+++ days/day04/b.ts
@@ -4,11 +4,48 @@ const input = await readInput(import.meta.dir);
 
 // Part B solution
 function solvePartB(data: string): number {
-  const lines = data.trim().split("\n");
+  const input = data
+    .trim()
+    .split("\n")
+    .filter((x) => x.length > 0)
+    .map((x) => x.split(""));
 
-  // Your solution here
+  let count = 0;
+  while (true) {
+    const removed = [];
+    for (let y = 0; y < input.length; y++) {
+      const row: string[] = input[y] || [];
+      const top: string[] = y > 0 ? input[y - 1] || [] : [];
+      const bottom: string[] = y < input.length - 1 ? input[y + 1] || [] : [];
+      for (let x = 0; x < row.length; x++) {
+        if (row[x] == "@") {
+          let rolls = 0;
+          const left = x - 1;
+          const right = x + 1;
+          if (top[left] == "@") rolls++;
+          if (top[x] == "@") rolls++;
+          if (top[right] == "@") rolls++;
+          if (row[left] == "@") rolls++;
+          if (row[right] == "@") rolls++;
+          if (bottom[left] == "@") rolls++;
+          if (bottom[x] == "@") rolls++;
+          if (bottom[right] == "@") rolls++;
+          if (rolls < 4) removed.push([x, y]);
+        }
+      }
+    }
+    for (const coordinate of removed) {
+      const x = coordinate[0];
+      const y = coordinate[1];
+      if (x !== undefined && y !== undefined && input[y]) {
+        input[y][x] = ".";
+      }
+    }
+    count += removed.length;
+    if (removed.length == 0) break;
+  }
 
-  return 0;
+  return count;
 }
 
 const answer = solvePartB(input);
